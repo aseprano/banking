2fdf92186037dd5436e467ccef992451228f0b63
MatteoMigliaccio <teo.migliaccio@gmail.com>
2022-07-01 09:41:56 +0200

    controllo eccezioni su concrete event bus

--- src/ConcreteEventBus.ts
+++ src/ConcreteEventBus.ts
@@ -7,20 +7,24 @@ export class ConcreteEventBus implements EventBus{
     subscribers : { [key: string]: Array<EventConsumer>; } = {};
 
     dispatch(event: Event): void {
-        var i : number;
         var eventSubscribers = [];
         eventSubscribers = this.subscribers[event.name];
 
         if(eventSubscribers !== undefined){
-            for(i = 0; i < eventSubscribers.length; i++){
-                eventSubscribers[i].onEvent(event);
+            for(let i = 0; i < eventSubscribers.length; i++){
+                try{
+                    eventSubscribers[i].onEvent(event);
+                }
+                catch(Exception){
+                    console.log("Exception throun");
+                }
             }
         }
     }
 
     subscribeTo(name: string, consumer: EventConsumer): void {
         if(this.subscribers[name] === undefined){
-            this.subscribers[name] = [consumer]
+            this.subscribers[name] = [consumer];
         }
         else{
             this.subscribers[name].push(consumer);
