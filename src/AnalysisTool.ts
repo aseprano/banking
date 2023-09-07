36fcc08d57ef22578e947079dd061254257de6b9
Giorgia <giorgiaiacono252005@gmail.com>
2023-09-07 17:50:21 +0200

    Application 3

--- src/AnalysisTool.ts
+++ src/AnalysisTool.ts
@@ -1,15 +1,30 @@
+import { Event } from "./Event";
+
 export class AnalysisTool
 {
     private balances=new Map<number,number>();
-    public onEvent(event: any):void
+    public onEvent(event: Event):void
     {
-        if(!this.balances.has())
+        switch(event.getName())
         {
-            this.balances.get();
-        }
-        else
-        {
-            
+            case "BankAccountCreated":
+                const id=event.getPayload().id??0;
+                this.balances.set(id,0);
+                break;
+            case "BankAccountCharged":
+                const id=event.getPayload().id??0;
+                const amount=event.getPayload().amount;
+                const currentBalance=this.balances.get(id)??0;
+                const newBalance=currentBalance-amount;
+                this.balances.set(id,newBalance);
+                break;
+            case "BankAccountDebited":
+                const id=event.getPayload().id??0;
+                const amount=event.getPayload().amount;
+                const currentBalance=this.balances.get(id)??0;
+                const newBalance=currentBalance+amount;
+                this.balances.set(id, newBalance);
+                break;
         }
     }
     public printList(): void
@@ -28,9 +43,10 @@ export class AnalysisTool
                 }
              }
         }
-        for(let i=0; i<entries.length; i++)
+        const maxEntries = entries.length < 10 ? entries.length : 10;
+        for(let i=0; i<maxEntries; i++)
         {
-            console.log(entries[i])
+            console.log(entries[i]);
         }
     }
 }
