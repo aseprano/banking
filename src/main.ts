58b794660146e1d14ca7a19e7920b777fe9e335d
Giorgia <giorgiaiacono252005@gmail.com>
2023-09-07 13:04:26 +0200

    Modifica Iniziale

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
-console.log("!!!!");
+var mat: number[][] = [][2];
+var bilancio: number = 0;
+
+//verifico presenza account
+//se non c'è stampa report
+// se c'è verifica cambiamenti
+
+//calcolo bilancio
+console.log(bilancio);
+
+//inseerisco bilancio nella matrice
+mat[1][2] = bilancio;
+
+//azzero bilancio
+bilancio = 0;
+
